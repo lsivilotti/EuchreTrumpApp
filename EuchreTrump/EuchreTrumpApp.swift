2583a58de5fb41c5c83629d940e89af077bb7324
lsivilotti <luca.sivilotti@gmail.com>
2024-03-28 14:57:51 -0400

    Initial Commit

---
+++ EuchreTrump/EuchreTrumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  EuchreTrumpApp.swift
+//  EuchreTrump
+//
+//  Created by Luca Sivilotti on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct EuchreTrumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
